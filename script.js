f9c7fce8725ea6a1fb7aa2b798e6082b05d632ac
Nareg-B-94 <naregb94@gmail.com>
2022-06-23 11:45:51 +0200

    more features like Disclaimer and so on

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ let labelsY = [];
 let myChart = null;
 let bitCoinCurrency = '';
 
+//loads the current Day for the date picker
+function todayAsFunction() {
+    ['startDate', 'endDate'].forEach((date) => {
+        document.getElementById(date).setAttribute('max', today);
+        document.getElementById(date).value = today;
+    });
+
+}
+
+//loads the current currency Value of Bitcoin
 async function loadCurrentRate() {
     let urlForToday = `https://data.nasdaq.com/api/v3/datasets/BCHAIN/MKPRU?start_date=${today}&end_date=${today}&api_key=${API_KEY}`;
     let responseToday = await fetch(urlForToday);
@@ -19,15 +29,16 @@ async function loadCurrentRate() {
     document.getElementById('currentCourse').innerHTML = bitCoinCurrency;
     document.getElementById('todaysDate').innerHTML = today;
     document.getElementById('currentCourse2').innerHTML = bitCoinCurrency;
+    todayAsFunction();
 }
 
-// Calculator 
-
+// Calculator --------------------------------------------------------------
+// calculates the value of Bitcoin to $
 function convertCurrencyToBTC() {
     let x = document.getElementById('input1').value / bitCoinCurrency;
     x = document.getElementById('input2').value = x.toFixed(6);
 }
-
+// calculates the value of $ to Bitcoin
 function convertCurrencyToUSD() {
     let x = document.getElementById('input2').value * bitCoinCurrency;
     document.getElementById('input1').value = x;
@@ -40,27 +51,29 @@ function removePopup() {
 
 
 
-// chart 
-
+// chart ----------------------------------------------------------------
+// saving the Dates to further creat the Chart 
 async function getDates() {
-    if (myChart != null) {
-        deleteChart()
-    }
-    if (document.getElementById('startDate').value.length == 0) {
-
-        document.getElementById('popupMessage').classList.remove('d-none');
-        return;
-    }
-    if (document.getElementById('endDate').value.length == 0) {
-
-        document.getElementById('popupMessage').classList.remove('d-none');
-        return;
-    }
-
-
+    let selectedStartDate = document.getElementById('startDate').value.length;
+    let selectedEndDate = document.getElementById('endDate').value.length;
     let startDateValue = document.getElementById('startDate').value;
     let endDateValue = document.getElementById('endDate').value;
 
+    if (myChart) {
+        deleteChart()
+    };
+    if (selectedStartDate == 0) {
+
+        document.getElementById('popupMessage').classList.remove('d-none');
+        return;
+    };
+    if (selectedEndDate == 0) {
+
+        document.getElementById('popupMessage').classList.remove('d-none');
+        return;
+    };
+
+    //continues with the data that are needed for creation of the Chart
 
     endDate.push(endDateValue);
     startDate.push(startDateValue);
@@ -85,8 +98,10 @@ function deleteChart() {
     labelsX = [];
 };
 
+
+// starts with the creation of the Chart 
 function chart() {
-    document.getElementById('chartContainer').classList.remove('d-none')
+    document.getElementById('chartContainer').classList.remove('d-none');
     let info = responseAsJson.dataset.data;
     for (let i = info.length - 1; i > 0; i--) {
         labelsX.push(info[i][0]);
@@ -114,6 +129,7 @@ function chart() {
 };
 
 
+// shows the created Chart as Bar-chart
 function getDatesBarChart() {
     if (myChart != null) {
         deleteChart()
@@ -128,7 +144,7 @@ function getDatesBarChart() {
         labels: labelsX, //labels = x Axis 
         datasets: [{
             label: 'Exchange Rate',
-            backgroundColor: 'gold',
+            backgroundColor: 'black',
             borderColor: 'black',
             data: labelsY, //data = Y Axis
         }]
@@ -146,6 +162,8 @@ function getDatesBarChart() {
 
 };
 
+// Table -------------------------------------------------------------------
+// checks if the table is build already
 function getDatesTable() {
     if (myChart != null) {
         deleteChart()
@@ -153,6 +171,8 @@ function getDatesTable() {
     buildTable()
 };
 
+
+// creates the Table as a data to scroll through
 function buildTable() {
     document.getElementById('tableOutput').style.display = 'grid'
     let tableData = document.getElementById('tableOutput');
@@ -166,26 +186,24 @@ function buildTable() {
             <td class="td1">${responseTable[i][0]}:</td> &nbsp 
             <td class="td2">${responseTable[i][1].toFixed(2)}&nbsp<b>USD</b></td>
         </tr>
-
     </table>
-
     `
     }
 };
 
-
+// limits the date picking to today in oppose to future
 $(function() {
-    var dtToday = new Date();
+    let dtToday = new Date();
 
-    var month = dtToday.getMonth() + 1;
-    var day = dtToday.getDate() - 1;
-    var year = dtToday.getFullYear();
+    let month = dtToday.getMonth() + 1;
+    let day = dtToday.getDate() - 1;
+    let year = dtToday.getFullYear();
     if (month < 10)
         month = '0' + month.toString();
     if (day < 10)
         day = '0' + day.toString();
 
-    var minDate = year + '-' + month + '-' + day;
+    let minDate = year + '-' + month + '-' + day;
 
     $('#endDate').attr('max', minDate);
     $('#startDate').attr('max', minDate);
